94e6117574f00206f4c96312227797b947428929
ogiwara <yushiogiwara@gmail.com>
2019-09-30 10:49:41 +0900

    reformat.

--- randomforestclass.py
+++ randomforestclass.py
@@ -6,20 +6,20 @@ from sklearn.ensemble import RandomForestClassifier
 data = pd.read_csv('ice.csv')
 # double rectangle!
 # x is inputs
-x=data[['temp','street']]
-y=data['ice']
+x = data[['temp', 'street']]
+y = data['ice']
 
 # 82 decision tree.
 # This is magic number (by takefuji.) Why? He don't know.
 # 最適な決定木の数がある
-clf=RandomForestClassifier(n_estimators=82, min_samples_split=2)
+clf = RandomForestClassifier(n_estimators=82, min_samples_split=2)
 # ML process is just here.
-clf.fit(x,y)
-print(clf.score(x,y))
+clf.fit(x, y)
+print(clf.score(x, y))
 print(clf.feature_importances_)
-p=clf.predict(x)
-t=np.arange(0.0,31.0)
-plt.plot(t,data['ice'],'--b')
-plt.plot(t,p,'-b')
-plt.legend(('real','randomF'))
+p = clf.predict(x)
+t = np.arange(0.0, 31.0)
+plt.plot(t, data['ice'], '--b')
+plt.plot(t, p, '-b')
+plt.legend(('real', 'randomF'))
 plt.show()
